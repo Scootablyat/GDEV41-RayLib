e957086f67fb3857f1fdefdd8d8a4283cd5629e5
John Tamano <john.tamano@obf.ateneo.edu>
2024-08-22 16:36:14 +0800

    added notes

--- helloraylib.cpp
+++ helloraylib.cpp
@@ -7,12 +7,26 @@
     replace helloraylib.cpp with .cpp file of choice (obviously)
     replace out with name of file (obviously)
 
+    DrawRectangle(x pos, y pos, width, height);
+    - (x, y) is where the rectangle will be draw from and it will act as the upper left point of the rectangle
+
+    ClearBackground(color);
+    - changes background color
+
     ./nameOfFile.exe
+
+
+    Raylib uses Polling based input (basically always checking a bunch of times a second if a button is being pressed)
 */
+
 int main(){
     InitWindow(800, 600, "Hello Raylib");
     while(!WindowShouldClose()){
         BeginDrawing();
+
+        ClearBackground(PINK);
+
+
         EndDrawing();
     }
     CloseWindow();
